088f7aa85bc9a65ea93c72adbaa40b06828f1b80
alexfull13 <apm.arias@yavirac.edu.ec>
2019-03-15 15:34:04 -0500

    ,j,
    ,ln nbmnnbm

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Http } from '@angular/http';
+
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableListComponent implements OnInit {
 
-  constructor() { }
+
+
+  
 
   ngOnInit() {
   }
