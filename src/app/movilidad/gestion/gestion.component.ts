affb5fffedcdd29e4f5ee6ef565f323436384d38
joseobando0001 <jose.obando_0001@hotmail.com>
2019-04-23 15:23:16 -0500

    Actualizacion de Vista de componentes

--- src/app/movilidad/gestion/gestion.component.ts
+++ src/app/movilidad/gestion/gestion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RequisitosService } from 'app/service/requisitos.service';
+import { ConsumirService } from 'app/service/consumir.service';
 
 @Component({
   selector: 'app-gestion',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestion.component.scss']
 })
 export class GestionComponent implements OnInit {
+  requisito: any;
+  id: any;
+  products: any = [];
 
-  constructor() { }
+
+  constructor(private router: Router, private requisitosService: RequisitosService, public rest: ConsumirService) { }
 
   ngOnInit() {
+    this.getrequisito();
   }
 
+  getProducts(id: number) {
+    this.products = [];
+    this.rest.getProducts(id).subscribe((data: {}) => {
+      console.log(data);
+      this.products = data;
+      this.products = Array.of(this.products);
+
+    });
+  }
+
+
+  getrequisito() {
+    // console.log('entre')
+    this.requisitosService.getrequisito().subscribe(data => {
+      if (data) {
+        console.log('requisito,data')
+        this.requisito = data;
+      }
+    });
+  }
 }
+
